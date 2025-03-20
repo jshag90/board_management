cf4c3b2f5a3ab2c24f42f61c3311efe3a60874cb
seonhakji <jshag1990@gmail.com>
2025-03-21 00:11:23 +0900

    [Bug] 게시판 첨부파일 삭제 쿼리 요청시 execute() 누락

--- src/main/java/com/rsupport/board/dao/NoticeDao.java
+++ src/main/java/com/rsupport/board/dao/NoticeDao.java
@@ -203,12 +203,12 @@ public class NoticeDao {
     public void removeAttachmentFile(Long postId, List<Long> removeAttachmentFileIdList) {
         jpaQueryFactory.delete(postAttachmentFile)
                 .where(postAttachmentFile.attachmentFileId.id.in(removeAttachmentFileIdList)
-                .and(postAttachmentFile.postId.eq(postId)));
+                .and(postAttachmentFile.postId.eq(postId))).execute();
 
         //해당 첨부파일을 매핑하고 있는 게시물이 없을 경우
         for (Long attachmentFileId : removeAttachmentFileIdList) {
             if (jpaQueryFactory.selectFrom(postAttachmentFile).where(postAttachmentFile.attachmentFileId.id.eq(attachmentFileId)).fetch().size() < 1) {
-                jpaQueryFactory.delete(attachmentFile).where(attachmentFile.id.in(removeAttachmentFileIdList));
+                jpaQueryFactory.delete(attachmentFile).where(attachmentFile.id.in(removeAttachmentFileIdList)).execute();
             }
         }
     }
