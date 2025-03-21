98e66f2509591e7cc9d12d252b8dd489df637ab9
seonhakji <jshag1990@gmail.com>
2025-03-21 16:57:25 +0900

    [Feat] 서버 기동시 관리자 계정 추가

--- src/main/java/com/rsupport/board/DatabaseDataLoader.java
+++ src/main/java/com/rsupport/board/DatabaseDataLoader.java
@@ -2,7 +2,9 @@ package com.rsupport.board;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.rsupport.board.entity.BoardType;
+import com.rsupport.board.entity.Member;
 import com.rsupport.board.utils.BoardTypeEnum;
+import com.rsupport.board.utils.StringUtil;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -10,9 +12,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 import static com.rsupport.board.entity.QBoardType.boardType;
+import static com.rsupport.board.entity.QMember.member;
+
 @Component
 @Slf4j
 @RequiredArgsConstructor
@@ -24,16 +29,25 @@ public class DatabaseDataLoader implements CommandLineRunner {
 
     @Transactional
     @Override
-    public void run(String... args) {
+    public void run(String... args) throws NoSuchAlgorithmException {
         List<BoardType> boardTypeList = jpaQueryFactory.selectFrom(boardType).fetch();
-
-        if(boardTypeList.size() < 1) {
+        if (boardTypeList.size() < 1) {
             for (BoardTypeEnum boardTypeEnum : BoardTypeEnum.values()) {
                 BoardType boardType = BoardType.builder().name(boardTypeEnum).build();
                 entityManager.persist(boardType);
             }
-            entityManager.flush();
         }
+
+        if (jpaQueryFactory.selectFrom(member).fetch().size() < 1) {
+            Member member = Member.builder()
+                                .loginId("admin")
+                                .password(StringUtil.textToSha256("abc123!@#"))
+                                .username("관리자")
+                                .role(Member.Role.ROLE_0)
+                                .build();
+            entityManager.persist(member);
+        }
+
     }
 
 }
