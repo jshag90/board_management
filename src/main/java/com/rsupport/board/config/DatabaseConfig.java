f548d3df1d2d98c5c555cdec90c526a5660c38df
seonhakji <jshag1990@gmail.com>
2025-03-24 15:15:07 +0900

    [Feat] 외부 경로 설정파일에서 값 읽어와서 datasource 초기화

---
+++ src/main/java/com/rsupport/board/config/DatabaseConfig.java
@@ -0,0 +1,46 @@
+package com.rsupport.board.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource(value = "file:${rs.install.config.file.path}")
+@RequiredArgsConstructor
+@Getter
+@Setter
+@Slf4j
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    String url;
+
+    @Value("${spring.datasource.username}")
+    String userName;
+
+    @Value("${spring.datasource.password}")
+    String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    String driverClassName;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(url);
+        hikariConfig.setUsername(userName);
+        hikariConfig.setPassword(password);
+        hikariConfig.setDriverClassName(driverClassName);
+        return new HikariDataSource(hikariConfig);
+    }
+
+}
