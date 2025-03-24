c7d018e3736ca39025899be530a9e8119cd6e097
seonhakji <jshag1990@gmail.com>
2025-03-24 20:17:22 +0900

    [Feat] 스웨거 return code 정의 추가

--- src/main/java/com/rsupport/board/dto/ResponseResultDto.java
+++ src/main/java/com/rsupport/board/dto/ResponseResultDto.java
@@ -1,5 +1,6 @@
 package com.rsupport.board.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,8 +10,15 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Builder
+@Schema(description = "에러 응답 DTO")
 public class ResponseResultDto<T> {
+
+    @Schema(description = "응답 코드 (0: 성공, -1: 서버 에러, -2: 유효하지 않은 파라미터, -3: 파일 용량 초과)", example = "0")
     Integer returnCode;
+
+    @Schema(description = "오류 메시지")
     String message;
+
+    @Schema(description = "추가 데이터")
     T data;
 }
